f78f3375731211a76f9c7b2b769f0bf881371af3
Dan Pina <dan@Duclearc-MBP.fritz.box>
2022-09-29 16:14:16 +0200

    [main] refactored to use environment variables

--- pages/api/bigquery.js
+++ pages/api/bigquery.js
@@ -1,39 +1,43 @@
-const credentials = require('../../config/ecomm-data-and-analytics-a7a255fb1e58.json')
-const { datasetId, tableId } = require('../../config/config.json')
-
 'use strict'
+
+const { client_email, dataset_id, private_key, project_id, table_id } = process.env
+
+if(!( client_email && dataset_id && private_key && project_id && table_id )) {
+    throw new Error('Environment Data missing')
+}
 
 // [START bigquery_client_json_credentials]
 // Creates a BigQuery client explicitly using service account credentials by specifying the private key file.
 const { BigQuery } = require('@google-cloud/bigquery')
 
 const options = {
-    keyFilename: './config/ecomm-data-and-analytics-a7a255fb1e58.json',
-    projectId: credentials.project_id
+    projectId: project_id,
+    credentials: {
+        client_email,
+        private_key
+    }
 }
 
 const bigquery = new BigQuery(options)
 // [END bigquery_client_json_credentials]
 
-async function loadDataToBigQuery(datasetId, tableId, data) {
+async function loadDataToBigQuery(dataset_id, table_id, payload) {
 
     // Loads data from a local file into the table
     await bigquery
-        .dataset(datasetId)
-        .table(tableId)
-        .insert(data, function (err, response) {
+        .dataset(dataset_id)
+        .table(table_id)
+        .insert(payload, function (err, response) {
             console.log("error:" + JSON.stringify(err));
             console.log("response:" + JSON.stringify(response));
         })
 }
 
 function processRequest({ body }) {
-    // console.log(method, {'data': target}, '\n')
 
-    loadDataToBigQuery(datasetId, tableId, body)
+    loadDataToBigQuery(dataset_id, table_id, body)
 
     return JSON.stringify({ version: 'Lemundo GmbH', message: 'data added', data: body })
-    // TODO: investigate if instead of a file we can upload the object directly
 
 }
 
